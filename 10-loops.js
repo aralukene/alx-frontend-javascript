28c7005d4e756ca538fa0c38c4a599c8ee54b533
aralukene <59538075+aralukene@users.noreply.github.com>
2023-09-25 10:03:27 +0100

    Delete 10-loops.js

--- 10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const strValue = [];
-  for (const idx of array) {
-    strValue.push(appendString + idx);
-  }
-
-  return strValue;
-}
