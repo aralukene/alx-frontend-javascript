b2b96f148547e5ac58bcc8220d09903657688a9d
aralukene <59538075+aralukene@users.noreply.github.com>
2023-09-25 10:05:19 +0100

    Delete 101-iterateThroughObject.js

--- 101-iterateThroughObject.js
+++
@@ -1,7 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  let employees = '';
-  for (const item of reportWithIterator) {
-    employees += `${item} | `;
-  }
-  return employees.slice(0, employees.length-3);
-}
